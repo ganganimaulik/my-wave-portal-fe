026d0fbbeaab33ea0f10d2bfaa6a5cbbe74b35c2
Maulik <ganganimaulik@gmail.com>
2022-06-03 08:35:09 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey world!
+        </div>
+
+        <div className="bio">
+        I am maulik and I worked on meeting assistant SaaS, so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
