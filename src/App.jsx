c361467082f735b13486a24fb4e65a43b1af7ac0
Maulik <ganganimaulik@gmail.com>
2022-06-05 13:37:35 +0000

    add event listener

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
   const [loading, setLoading] = useState(false);
   const [totalWaves, setTotalWaves] = useState(null);
-  const contractAddress = "0xcFBACDCEc40e66F7ED695478293840c0E35eB7Db";
+  const contractAddress = "0x07E950C2287a5d49b6B0F6BDC37e5C7b16d727aD";
   const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
@@ -101,7 +101,35 @@ export default function App() {
   
   useEffect(() => {
     checkIfWalletIsConnected();
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves(prevState => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+  
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+  
+      wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+  
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
   }, []);
+  
   const getTotalWaves = async () => {
     try {
       const { ethereum } = window;
@@ -136,7 +164,7 @@ export default function App() {
         console.log("Retrieved total wave count...", count.toNumber());
         setTotalWaves(count);
 
-        const waveTxn = await wavePortalContract.wave(waveInputMsg);
+        const waveTxn = await wavePortalContract.wave(waveInputMsg, { gasLimit: 300000 });
         console.log("Mining...", waveTxn.hash);
         setLoading(true);
 
@@ -146,11 +174,11 @@ export default function App() {
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
         setTotalWaves(count);
-        getAllWaves();
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
+      alert(error.message);
       console.log(error);
     }
     setLoading(false);
@@ -162,15 +190,15 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey world!
+        👋 Hello world!
         </div>
 
         <div className="bio">
-        I am maulik and I worked on meeting assistant SaaS app Bluecap.ai? Connect your Ethereum wallet and wave at me!
+        <strong>I am maulik</strong> - fullstack MERN developer. I have worked on meeting assistant SaaS app Bluecap.ai - Connect your Ethereum wallet and wave at me!
         </div>
         <form onSubmit={wave} style={{textAlign: 'center', marginTop: '15px'}}>
           <div>
-            <textarea disabled={loading} value={waveInputMsg} onChange={(e) => setWaveInputMsg(e.target.value)} rows={3} required style={{width: '100%'}}/>
+            <textarea placeholder='enter your message..' disabled={loading} value={waveInputMsg} onChange={(e) => setWaveInputMsg(e.target.value)} rows={3} required style={{width: '100%'}}/>
           </div>
           <button disabled={loading} className="waveButton" type="submit">
             Wave at Me
@@ -192,9 +220,9 @@ export default function App() {
         {allWaves.map((wave, index) => {
           return (
             <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
-              <div>Address: {wave.address}</div>
-              <div>Time: {wave.timestamp.toString()}</div>
-              <div>Message: {wave.message}</div>
+              <div>From Address: {wave.address}</div>
+              <div>Time: <em>{wave.timestamp.toString()}</em></div>
+              <div>Message: <strong>{wave.message}</strong></div>
             </div>)
         })}
       </div>
